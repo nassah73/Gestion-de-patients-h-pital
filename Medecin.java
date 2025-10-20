1955465fa2a922a72b3dc6917a6e29242687e0a5
hassan <hassansobai6@gmail.com>
2025-10-20 21:11:26 +0200

    added class Medecin

---
+++ Medecin.java
@@ -0,0 +1,11 @@
+public class Medecin {
+    private String name;
+    private String specialty;
+    public Medecin(String name, String specialty) {
+        this.name = name;
+        this.specialty = specialty;
+    }
+    public void prend_un_rendez_vous(){
+        System.out.println("Le médecin " + name + " spécialisé en " + specialty + " a pris un rendez-vous.");
+    }
+}
